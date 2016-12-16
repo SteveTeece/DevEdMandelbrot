83ce2e0fb4734d7c98748300c71197a0d52530c0
Richard Green <richard.green@15below.com>
2016-12-16 13:34:30 +0000

    First commit

---
+++ DevEdMandelbrot.Messages/Request.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Drawing;
+
+namespace DevEdMandelbrot.Messages
+{
+    public class Request
+    {
+        public Guid CorrelationId;
+
+        /// <summary>
+        /// The bounding box of the mandelbrot space
+        /// </summary>
+        public Viewport Viewport;
+
+        /// <summary>
+        /// The region of the bitmap required to be rendered
+        /// </summary>
+        public Rectangle RenderArea;
+
+        /// <summary>
+        /// The width of the full bitmap
+        /// </summary>
+        public int TotalWidth;
+
+        /// <summary>
+        /// The height of the full bitmap
+        /// </summary>
+        public int TotalHeight;
+    }
+
+    public class Viewport
+    {
+        public double StartX;
+        public double StartY;
+        public double EndX;
+        public double EndY;
+    }
+}
---
+++ DevEdMandelbrot.Messages/Response.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DevEdMandelbrot.Messages
+{
+    public class Response
+    {
+        public Guid CorrelationId;
+
+        /// <summary>
+        /// The binary data of the bitmap you rendered. See the ImageConverter class, function ConvertTo.
+        /// </summary>
+        public byte[] ImageData;
+
+        /// <summary>
+        /// Who rendered this?
+        /// </summary>
+        public string ServerName;
+
+        /// <summary>
+        /// How long did it take you to render this?
+        /// </summary>
+        public long RenderTimeMs;
+    }
+}
